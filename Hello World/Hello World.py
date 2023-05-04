6c5bc758c9ac6dfc58b9a153913c18116a89fcc9
TRILOCHNA <vaidtrilochna@gmail.com>
2023-05-04 22:21:21 +0530

    Create Hello World.py

---
+++ Hello World/Hello World.py
@@ -0,0 +1,12 @@
+"""
+Hello, world!
+Your first python program looks a lot like Karel!
+"""
+
+def main():
+    # This prints to the console
+    print("Hello, World!")
+
+# Run the main function when this file is executed
+if __name__ == '__main__':
+    main()
