eec5784d4188c5441672bcd7c40fdc43d5768628
TRILOCHNA <vaidtrilochna@gmail.com>
2023-05-05 15:30:25 +0530

    Create Hello Name.py

---
+++ Hello Name/Hello Name.py
@@ -0,0 +1,10 @@
+def main():
+    """
+    You should write your code here. Make sure to delete 
+    the 'pass' line before starting to write your own code.
+    """
+name = input("What is your name? ")
+print("Hello " + name)
+
+if __name__ == '__main__':
+    main()
