16317b35aec57e0cdfacad5b8e191f70630ea696
TRILOCHNA <vaidtrilochna@gmail.com>
2023-05-09 09:27:31 +0530

    Create Random Numbers.py

---
+++ Random Numbers/Random Numbers.py
@@ -0,0 +1,13 @@
+# this is necessary if you want to use random numbers
+import random
+
+def main():
+    """
+    You should write your code here. 
+    """
+for i in range(10):
+    value = random.randint(1, 100)
+    print(value)
+
+if __name__ == '__main__':
+    main()
