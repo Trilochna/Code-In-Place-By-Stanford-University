d06efa008cb07abdde78364a087c208979b2bfab
TRILOCHNA <vaidtrilochna@gmail.com>
2023-06-04 23:15:21 +0530

    Create Print Evens.py

---
+++ Print Evens/Print Evens.py
@@ -0,0 +1,8 @@
+def main():
+    for i in range(20):
+        # use the value inside the for loop
+        print(i * 2)
+   
+# call the main function when "run" 
+if __name__ == "__main__":
+    main()
